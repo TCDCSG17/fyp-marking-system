40deed0882d789a4b6746325fc5d71485f388808
clintonwh <clintonw@tcd.ie>
2016-03-08 13:26:46 +0000

    Small changes so main page brings you to the students page

--- fyp-marking-system/config/routes.rb
+++ fyp-marking-system/config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'welcome' => 'pages#home'
+  get 'pages/home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
