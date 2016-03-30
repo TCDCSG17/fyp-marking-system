f980b5e67cb4c88eb50795a2c507cd7e848da2c9
clintonwh <clintonw@tcd.ie>
2016-03-30 13:40:17 +0100

    Localhost:3000 is main page

--- fyp-marking-system/config/routes.rb
+++ fyp-marking-system/config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   resources :students
-  get 'welcome/index'
+  get 'students/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'students#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
