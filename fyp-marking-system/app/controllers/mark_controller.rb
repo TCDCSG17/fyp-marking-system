6f16c907d222dce130f6873ad43209d1e5d42745
Gavin Corkery <gavcorkery@gmail.com>
2016-03-30 14:04:31 +0100

    Added preliminary mark page (no styles yet)

---
+++ fyp-marking-system/app/controllers/mark_controller.rb
@@ -0,0 +1,2 @@
+class MarkController < ApplicationController
+end
---
+++ fyp-marking-system/test/controllers/mark_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class MarkControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
