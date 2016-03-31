e298694450cd3e0cb5cd5d5d42bef94cc2ad776a
Emmet Broaders <broadere@tcd.ie>
2016-03-31 17:01:38 +0100

    added the marker MCV

--- fyp-marking-system/app/controllers/marks_controller.rb
+++ fyp-marking-system/app/controllers/marks_controller.rb
@@ -1,2 +1,12 @@
 class MarksController < ApplicationController
+	def create
+		@student = Student.find(params[:student_id])
+		@mark = @student.marks.create(params[:input1, :input2, :input3, :weight1, :weight2, :weight3, :mark1, :mark2, :mark3, :final_mark].permit(:input1, :input2, :input3, :weight1, :weight2, :weight3, :mark1, :mark2, :mark3, :final_mark))
+
+		if @mark.save
+			redirect_to student_path(@student)
+		else
+			render 'new'
+		end
+	end
 end
