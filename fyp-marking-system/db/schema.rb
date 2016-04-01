e15249e16753783db497c8176136cab0b0af11ef
clintonwh <clintonw@tcd.ie>
2016-04-01 11:42:33 +0100

    Put in Leah's work that was overwritten

--- fyp-marking-system/db/schema.rb
+++ fyp-marking-system/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160331173528) do
+ActiveRecord::Schema.define(version: 20160330212018) do
 
   create_table "marks", force: :cascade do |t|
     t.text     "input1"
